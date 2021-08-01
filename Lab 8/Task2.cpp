221c6fef0311b95af04e82cdc3fcc7b9805c8809
Sumit Alam Khan <31077557+Tsumiiiiiiii@users.noreply.github.com>
2021-08-02 00:34:49 +0600

    Create Task2.cpp

---
+++ Lab 8/Task2.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+
+using namespace std;
+
+class Seat {
+protected:
+  bool pleasant, comfy, seatWarmer;
+public:
+  Seat(bool _pleasant = false, bool _comfy = false, bool _seatWarmer = false) : pleasant(_pleasant), comfy(_comfy), seatWarmer(_seatWarmer) {}
+  void getSeat() const {
+    cout << "Showing seat info : ";
+    cout << pleasant << " " << comfy << " " << seatWarmer << "\n";
+  }
+  void setSeat() {
+    cout << "Enter seat info: ";
+    cin >> pleasant >> comfy >> seatWarmer;
+  }
+};
+
+class Window {
+protected:
+  double circumference;
+public:
+  Window(double _circumference = 0) : circumference(_circumference) {}
+  void getWindow() const {
+    cout << "Showing window info : ";
+    cout << circumference << "\n";
+  }
+  void setWindow() {
+    cout << "Enter window info: ";
+    cin >> circumference;
+  }
+};
+
+class Engine {
+protected:
+  double maxFCR, maxEPR, avgRPM;
+public:
+  Engine (double _maxFCR = 0, double _maxEPR = 0, double _avgRPM = 0) : maxFCR(_maxFCR), maxEPR(_maxEPR), avgRPM(_avgRPM) {}
+  void getEngine() const {
+    cout << "Showing engine info : ";
+    cout << maxFCR << " " << maxEPR << " " << avgRPM << "\n";
+  }
+  void setEngine() {
+    cout << "Enter Engine info: ";
+    cin >> maxFCR >> maxEPR >> avgRPM;
+  }
+};
+
+class Door {
+protected:
+  string openingWay;
+public:
+  Door(string _openingWay = "") : openingWay(_openingWay) {}
+  void getDoor() const {
+    cout << "Showing door info : ";
+    cout << openingWay << "\n";
+  }
+  void setDoor() {
+    cout << "Enter door info: ";
+    cin >> openingWay;
+  }
+};
+
+class Car : public Seat, public Window, public Engine, public Door {
+private:
+  double max_accelaration, fuel_capacity;
+public:
+  Car(double _max_accelaration = 0, double _fuel_capacity = 0) : max_accelaration(_max_accelaration), fuel_capacity(_fuel_capacity) {}
+  void setCar() {
+    cout << "Enter car info: ";
+    cin >> max_accelaration >> fuel_capacity;
+  }
+  void getCar() const {
+    cout << "Car info: ";
+    cout << max_accelaration << "\n";
+  }
+  void get() const{
+    cout << "All the relevant info per category:\n";
+    getSeat();
+    getWindow();
+    getEngine();
+    getDoor();
+    getCar();
+  }
+  void set() {
+    cout << "Enter all the relevant info as requested:\n";
+    setSeat();
+    setWindow();
+    setEngine();
+    setDoor();
+  }
+};
+
+int main() {
+  Car Lambo(14.7, 714714.14);
+  Lambo.set();
+  Lambo.get();
+  return 0;
+}
