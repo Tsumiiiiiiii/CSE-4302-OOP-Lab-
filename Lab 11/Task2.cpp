1426c6039579867b5429295c5383a7c5763ce224
Sumit Alam Khan <31077557+Tsumiiiiiiii@users.noreply.github.com>
2021-10-04 11:51:18 +0600

    Update Task2.cpp

--- Lab 11/Task2.cpp
+++ Lab 11/Task2.cpp
@@ -41,18 +41,18 @@ employee_type employee::get_type() {
   else if( typeid(*this)==typeid(laborer) )
     return tlaborer;
   else
-    {cerr << “\nBad employee type”; exit(1); }
+    cerr << "\nBad type\n";
   return tmanager;
 }
 
 void employee::write() {
   int size;
-  cout << “Writing “ << n << “ employees.\n”;
+  //cout << “Writing “ << n << “ employees.\n”;
   ofstream ouf; //open ofstream in binary
   employee_type etype; //type of each employee object
-  ouf.open(“EMPLOY.DAT”, ios::trunc | ios::binary);
+  ouf.open("EMPLOY.DAT", ios::trunc | ios::binary);
   if(!ouf)
-    { cout << “\nCan’t open file\n”; return; }
+    { cout << "\nCan’t open file\n"; return; }
   for(int j=0; j<n; j++) { //for every employee object get its type
     etype = arrap[j]->get_type(); //write type to file
     ouf.write( (char*)&etype, sizeof(etype) );
@@ -62,7 +62,7 @@ void employee::write() {
     } //write employee object to file
   ouf.write( (char*)(arrap[j]), size );
   if(!ouf)
-    { cout << “\nCan’t write to file\n”; return; }
+    { cout << "\nCan’t write to file\n"; return; }
   }
 }
 
@@ -70,16 +70,16 @@ void employee::read() {
   int size; //size of employee object
   employee_type etype; //type of employee
   ifstream inf; //open ifstream in binary
-  inf.open(“EMPLOY.DAT”, ios::binary);
+  inf.open("EMPLOY.DAT", ios::binary);
   if(!inf)
-    {cout << “\nCan’t open file\n”; return; }
+    {cout << "\nFailed to open file\n" ;return; }
   n = 0; //no employees in memory yet
   while(true) { //read type of next employee
     inf.read( (char*)&etype, sizeof(etype) );
     if(inf.eof() ) //quit loop on eof
       break;
     if(!inf) //error reading type
-      { cout << “\nCan’t read type from file\n”; return; }
+      { cout << "\nCan’t read type from file\n"; return; }
     switch(etype) { //make new employee
       case tmanager: //of correct type
         arrap[n] = new manager;
@@ -89,14 +89,14 @@ void employee::read() {
         arrap[n] = new laborer;
         size = sizeof(laborer);
         break;
-      default: cout << “\nUnknown type in file\n”; return;
+      default: cout << "\nUnknown type in file\n"; return;
     } //read data from file into it
   inf.read( (char*)arrap[n], size );
   if(!inf) //error but not eof
-    { cout << “\nCan’t read data from file\n”; return; }
+    { cout << "\nCan’t read data from file\n"; return; }
   n++; //count employee
   } //end while
-  cout << “Reading “ << n << “ employees\n”;
+  cout << "Reading “ << n << “ employees\n";
 }
 
 int main() {
