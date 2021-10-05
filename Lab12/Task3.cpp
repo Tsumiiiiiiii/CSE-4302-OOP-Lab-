89168c0ab72e0d6e939831af07fd36aa8b50146b
Sumit Alam Khan <31077557+Tsumiiiiiiii@users.noreply.github.com>
2021-10-05 11:19:46 +0600

    Update Task3.cpp

--- Lab12/Task3.cpp
+++ Lab12/Task3.cpp
@@ -1,39 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-const int LIMIT = 10; 
+const int LIMIT = 10;
 
 template<class T>
 class safearay {
 private:
-  T arr[LIMIT];
+  T a[LIMIT];
 public:
+  class OutOfBound {
+  public:
+    int idx;
+    OutOfBound(int idx) : idx(idx) {}
+  };
   T& operator [](int n) { //note: return by reference
-    if(n< 0 || n>=LIMIT) {
-      cout << "\nIndex out of bounds";
-      exit(1);
+    if( n< 0 || n>=LIMIT ) {
+      throw OutOfBound(n);
     }
-    return arr[n];
+    return a[n];
   }
 };
 
 int main() {
 
-  safearay<int> sa1;
-  for(int i = 0; i < LIMIT; i++) 
-    sa1[i] = i; 
-  for(int i = 0; i < LIMIT; i++) {
-    int temp1 = sa1[i];
-    cout << "Element " << i << " is " << temp1 << "\n";
+  try {
+    safearay<int> sa1;
+    for(int i=0; i<LIMIT+5; i++) //insert elements
+      sa1[i] = i; //*left* side of equal sign
+    for(int i=0; i<LIMIT; i++) { //display elements
+      int temp1 = sa1[i]; //*right* side of equal sign
+      cout << "Element " << i << " is " << temp1 << endl;
+    }
+
+  }
+  catch(safearay<int>::OutOfBound e) {
+    cout<<"Out of Bound encountered at index : "<< e.idx <<endl;
   }
 
-  safearay<double> sa2;
-  for(int i=0;i<LIMIT;i++) {
-    sa2[i]= i * 0.5;
-  }
-  for(int i=0; i<LIMIT; i++) {
-    cout << "Element " << i << " is " << sa2[i] << "\n";
-  }
-  return 0;
+
+    return 0;
 }
